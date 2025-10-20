74d7a6ffc3ca38283a89ec4a1124e51f3226ed87
Tshepiso Sinyosi <ST10457536@imconnect.edu.za>
2025-10-20 23:23:11 +0200

    Added the classes with thier respective code such as the (Claim ,Document and the user classes).

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContractClaimSystem.Data
+{
+    public class ApplicationDbContext : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
