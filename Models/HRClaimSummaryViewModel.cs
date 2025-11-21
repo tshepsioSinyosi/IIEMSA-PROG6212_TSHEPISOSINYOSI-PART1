7707fd03a4fe531f4268df20ee371f727eba46ba
Tshepiso Sinyosi <ST10457536@imconnect.edu.za>
2025-11-21 16:24:19 +0200

    Added the Report View To be live in the HR View

--- Models/HRClaimSummaryViewModel.cs
+++ Models/HRClaimSummaryViewModel.cs
@@ -2,5 +2,8 @@
 {
     public class HRClaimSummaryViewModel
     {
+        public string LecturerName { get; set; } = "";
+        public decimal TotalHours { get; set; }
+        public decimal TotalPayment { get; set; }
     }
 }
